14a03646073d9afadd31393035350157877e9215
R533-Code <raphaeldibnehmepc@gmail.com>
2025-11-13 18:11:11 +0100

    Added include for FreeListAllocator

--- src/allocators/tests/test_allocators.cpp
+++ src/allocators/tests/test_allocators.cpp
@@ -2,6 +2,7 @@
 #include <stdcolt_allocators/allocators/null_allocator.h>
 #include <stdcolt_allocators/allocators/stack_allocator.h>
 #include <stdcolt_allocators/allocators/mallocator.h>
+#include <stdcolt_allocators/allocators/free_list_allocator.h>
 
 TEST_CASE("stdcolt/alloc")
 {
