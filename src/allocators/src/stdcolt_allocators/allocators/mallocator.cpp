0aa06f315e0c16633ee2b365ff436e5be74e54ce
R533-Code <raphaeldibnehmepc@gmail.com>
2025-11-13 11:40:25 +0100

    Added MallocatorAligned

--- src/allocators/src/stdcolt_allocators/allocators/mallocator.cpp
+++ src/allocators/src/stdcolt_allocators/allocators/mallocator.cpp
@@ -6,17 +6,66 @@
  * @date   November 2025
  *********************************************************************/
 #include <stdcolt_allocators/allocators/mallocator.h>
+#include <cstddef>
 #include <cstdlib>
+
+#if defined(_WIN32)
+  #include <malloc.h> // _aligned_malloc, _aligned_free
+#else
+  #include <errno.h>
+  #include <stdlib.h> // posix_memalign, free
+#endif
 
 namespace stdcolt::alloc
 {
-  Block Mallocator::allocate(size_t size) noexcept
+  Block Mallocator::allocate(Layout request) const noexcept
   {
-    return {std::malloc(size), size};
+    if (request.align() > PREFERRED_ALIGNMENT)
+      return nullblock;
+    return {std::malloc(request.size()), request.size()};
   }
 
-  void Mallocator::deallocate(Block blk) noexcept
+  void Mallocator::deallocate(Block blk) const noexcept
   {
     std::free(blk.ptr());
+  }
+
+  static void* aligned_malloc(size_t size, size_t alignment)
+  {
+    if (alignment < PREFERRED_ALIGNMENT)
+      alignment = PREFERRED_ALIGNMENT;
+
+#if defined(_WIN32)
+    return _aligned_malloc(size, alignment);
+#else
+    void* p = nullptr;
+    int rc  = posix_memalign(&p, alignment, size);
+
+    if (rc != 0)
+      return nullptr;
+    return p;
+#endif
+  }
+
+  static void aligned_free(void* ptr) noexcept
+  {
+    if (ptr == nullptr)
+      return;
+
+#if defined(_WIN32)
+    _aligned_free(ptr);
+#else
+    std::free(ptr);
+#endif
+  }
+
+  Block MallocatorAligned::allocate(Layout request) const noexcept
+  {
+    return {aligned_malloc(request.size(), request.align()), request.size()};
+  }
+
+  void MallocatorAligned::deallocate(Block blk) const noexcept
+  {
+    aligned_free(blk.ptr());
   }
 } // namespace stdcolt::alloc
--- src/allocators/src/stdcolt_allocators/allocators/mallocator.h
+++ src/allocators/src/stdcolt_allocators/allocators/mallocator.h
@@ -1,6 +1,6 @@
 /*****************************************************************/ /**
  * @file   mallocator.h
- * @brief  Contains `Mallocator`.
+ * @brief  Contains `Mallocator` and `AlignedMallocator`.
  * 
  * @author Raphael Dib Nehme
  * @date   November 2025
@@ -13,7 +13,9 @@
 
 namespace stdcolt::alloc
 {
-  /// @brief Allocator wrapper over `malloc` and `free`
+  /// @brief Allocator wrapper over `malloc` and `free`.
+  /// This allocator does not support extended alignment, use `AlignedMallocator`
+  /// for that.
   struct Mallocator
   {
     static constexpr AllocatorInfo allocator_info = {
@@ -21,18 +23,46 @@ namespace stdcolt::alloc
         .is_fallible         = true,
         .is_nothrow_fallible = true,
         .returns_exact_size  = true,
+        .alignment           = PREFERRED_ALIGNMENT,
     };
 
     STDCOLT_ALLOCATORS_EXPORT
     /// @brief Allocates a block using `malloc`
-    /// @param size The size of the block
+    /// @param request The allocation request
     /// @return The block or nullblock on failure
-    Block allocate(size_t size) noexcept;
+    Block allocate(Layout request) const noexcept;
 
     STDCOLT_ALLOCATORS_EXPORT
     /// @brief Deallocates a block
     /// @param blk The block to deallocate
-    void deallocate(Block blk) noexcept;
+    void deallocate(Block blk) const noexcept;
+  };
+  static_assert(IsAllocator<Mallocator>);
+
+  /// @brief Allocator wrapper over aligned `malloc` and aligned `free`.
+  /// This allocator supports extended alignment. If the underlying OS
+  /// cannot allocate a block with that specific alignment, `nullblock`
+  /// is returned.
+  struct MallocatorAligned
+  {
+    static constexpr AllocatorInfo allocator_info = {
+        .is_thread_safe      = true,
+        .is_fallible         = true,
+        .is_nothrow_fallible = true,
+        .returns_exact_size  = true,
+        .alignment           = PREFERRED_ALIGNMENT,
+    };
+
+    STDCOLT_ALLOCATORS_EXPORT
+    /// @brief Allocates a block using `malloc`
+    /// @param request The allocation request
+    /// @return The block or nullblock on failure
+    Block allocate(Layout request) const noexcept;
+
+    STDCOLT_ALLOCATORS_EXPORT
+    /// @brief Deallocates a block
+    /// @param blk The block to deallocate
+    void deallocate(Block blk) const noexcept;
   };
   static_assert(IsAllocator<Mallocator>);
 } // namespace stdcolt::alloc
