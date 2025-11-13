fea215891635b72e1c872e0cb5071ea32dc0d26a
R533-Code <raphaeldibnehmepc@gmail.com>
2025-11-13 11:41:03 +0100

    Added empty test

---
+++ src/allocators/tests/test_allocators.cpp
@@ -0,0 +1,9 @@
+#include <doctest/doctest.h>
+#include <stdcolt_allocators/allocators/null_allocator.h>
+#include <stdcolt_allocators/allocators/stack_allocator.h>
+#include <stdcolt_allocators/allocators/mallocator.h>
+
+TEST_CASE("stdcolt/alloc")
+{
+
+}
