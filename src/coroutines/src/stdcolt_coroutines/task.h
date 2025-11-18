82441753dc3b9c63609c4e57a4ec14c03723285a
R533-Code <raphaeldibnehmepc@gmail.com>
2025-11-18 13:44:59 +0100

    Improved readability and factored out some code

--- src/coroutines/src/stdcolt_coroutines/task.h
+++ src/coroutines/src/stdcolt_coroutines/task.h
@@ -1,6 +1,6 @@
 /*****************************************************************/ /**
  * @file   task.h
- * @brief  Contains `Task<T>`
+ * @brief  Contains `Task<T>`, and other task-related utilities.
  * 
  * @author Raphael Dib Nehme
  * @date   November 2025
@@ -16,175 +16,226 @@
 
 namespace stdcolt::coroutines
 {
-  template<typename T>
-  class Task;
+  //---------------------------------
+  // COMMON UTILITIES FOR ALL TASKS
+  //---------------------------------
 
-  namespace detail
+  /// @brief Trait to specify the return object type for `get_return_object`.
+  /// @tparam PromiseBase The promise base for which to use
+  template<typename PromiseBase>
+  struct return_object_for_promise_base;
+
+  /// @brief Generic promise for task storage.
+  /// @tparam T The result type to store
+  /// @tparam PROMISE_BASE The promise base class to inherit for extra-data
+  template<typename T, typename PROMISE_BASE>
+  class generic_task_promise final : public PROMISE_BASE
   {
-    class TaskPromiseBase
+    /// @brief The state of the storage
+    enum class StorageState : uint8_t
     {
-      std::coroutine_handle<> _continuation;
-
-      friend struct final_awaitable;
-
-      struct final_awaitable
-      {
-        bool await_ready() const noexcept { return false; }
-
-        template<typename PROMISE>
-        std::coroutine_handle<> await_suspend(
-            std::coroutine_handle<PROMISE> handle) noexcept
-        {
-          auto ret = handle.promise()._continuation;
-          return ret ? ret : std::noop_coroutine();
-        }
-
-        void await_resume() noexcept {}
-      };
-
-    public:
-      TaskPromiseBase() noexcept {}
-
-      auto initial_suspend() noexcept { return std::suspend_always{}; }
-      auto final_suspend() noexcept { return final_awaitable{}; }
-      void set_continuation(std::coroutine_handle<> continuation) noexcept
-      {
-        _continuation = continuation;
-      }
+      /// @brief Nothing
+      EMPTY,
+      /// @brief Value
+      VALUE,
+      /// @brief Exception pointer
+      EXCEPT,
     };
 
-    template<typename T>
-    class TaskPromise final : public TaskPromiseBase
+    /// @brief Storage suitable for storing the produced value or the exception
+    alignas(std::max(alignof(T), alignof(std::exception_ptr))) char storage[std::max(
+        sizeof(T), sizeof(std::exception_ptr))];
+    /// @brief The state of the storage
+    StorageState state = StorageState::EMPTY;
+
+    /// @brief Returns the exception pointer
+    /// @pre `state == StorageState::EXCEPT`
+    /// @return The exception pointer
+    std::exception_ptr& get_exception() noexcept
     {
-      /// @brief The state of the storage
-      enum class StorageState : uint8_t
-      {
-        /// @brief Nothing
-        EMPTY,
-        /// @brief Value
-        VALUE,
-        /// @brief Exception pointer
-        EXCEPT,
-      };
+      STDCOLT_debug_pre(state == StorageState::EXCEPT, "expected exception");
+      return *(std::exception_ptr*)storage;
+    }
+    /// @brief Returns the value
+    /// @pre `state == StorageState::VALUE`
+    /// @return The value
+    T& get_value() noexcept
+    {
+      STDCOLT_debug_pre(state == StorageState::VALUE, "expected value");
+      return *(T*)storage;
+    }
 
-      /// @brief Storage suitable for storing the produced value or the exception
-      alignas(std::max(alignof(T), alignof(std::exception_ptr))) char storage
-          [std::max(sizeof(T), sizeof(std::exception_ptr))];
-      /// @brief The state of the storage
-      StorageState state = StorageState::EMPTY;
+  public:
+    /// @brief Constructor, initializes to an empty state
+    generic_task_promise() noexcept = default;
+    /// @brief Destructor, destroys the value if there is one
+    ~generic_task_promise()
+    {
+      if (state == StorageState::VALUE)
+        get_value().~T();
+      else if (state == StorageState::EXCEPT)
+        get_exception().~exception_ptr();
+    }
 
-      std::exception_ptr& get_exception() noexcept
+    /// @brief The type to construct in `get_return_object`
+    using return_type =
+        typename return_object_for_promise_base<PROMISE_BASE>::template type<T>;
+
+    /// @brief Creates the return object using `return_object_for_promise_base`
+    /// @return The return object, constructed from the handle
+    return_type get_return_object() noexcept
+    {
+      using promise_t = generic_task_promise<T, PROMISE_BASE>;
+      auto h          = std::coroutine_handle<promise_t>::from_promise(*this);
+      return return_type{h};
+    }
+    /// @brief On unhandled exceptions, capture to exception in the storage
+    void unhandled_exception() noexcept
+    {
+      STDCOLT_debug_assert(state == StorageState::EMPTY, "Task must be empty");
+      // as Task must only co_return, not co_yield ^^^
+      new (storage) std::exception_ptr(std::current_exception());
+      state = StorageState::EXCEPT;
+    }
+
+    /// @brief Returns a value from the coroutine
+    /// @tparam U The type from which to convert
+    /// @param value The value to return
+    template<std::convertible_to<T> U>
+    void return_value(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+    {
+      STDCOLT_debug_assert(state == StorageState::EMPTY, "Task must be empty");
+      // as Task must only co_return, not co_yield ^^^
+      try
       {
-        STDCOLT_debug_pre(state == StorageState::EXCEPT, "expected exception");
-        return *(std::exception_ptr*)storage;
+        new (storage) T(std::forward<U>(value));
+        state = StorageState::VALUE;
       }
-      T& get_value() noexcept
+      catch (...)
       {
-        STDCOLT_debug_pre(state == StorageState::VALUE, "expected value");
-        return *(T*)storage;
-      }
-
-    public:
-      TaskPromise() noexcept = default;
-
-      ~TaskPromise()
-      {
-        if (state == StorageState::VALUE)
-          get_value().~T();
-        else if (state == StorageState::EXCEPT)
-          get_exception().~exception_ptr();
-      }
-
-      Task<T> get_return_object() noexcept;
-
-      void unhandled_exception() noexcept
-      {
-        STDCOLT_debug_assert(state == StorageState::EMPTY, "Task must be empty");
-        // as Task must only co_return, not co_yield ^^^
         new (storage) std::exception_ptr(std::current_exception());
         state = StorageState::EXCEPT;
       }
+    }
 
-      template<std::convertible_to<T> U>
-      void return_value(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
-      {
-        STDCOLT_debug_assert(state == StorageState::EMPTY, "Task must be empty");
-        // as Task must only co_return, not co_yield ^^^
-        try
-        {
-          new (storage) T(std::forward<U>(value));
-          state = StorageState::VALUE;
-        }
-        catch (...)
-        {
-          new (storage) std::exception_ptr(std::current_exception());
-          state = StorageState::EXCEPT;
-        }
-      }
-
-      T& result() &
-      {
-        if (state == StorageState::EXCEPT)
-          std::rethrow_exception(get_exception());
-        return get_value();
-      }
-
-      T&& result() &&
-      {
-        if (state == StorageState::EXCEPT)
-          std::rethrow_exception(get_exception());
-        return std::move(get_value());
-      }
-    };
-
-    template<>
-    class TaskPromise<void> : public TaskPromiseBase
+    /// @brief Returns the result, or throws the unhandled exception
+    /// @return The returned object
+    T& result() &
     {
-      std::exception_ptr exception;
+      if (state == StorageState::EXCEPT)
+        std::rethrow_exception(get_exception());
+      return get_value();
+    }
 
-    public:
-      TaskPromise() noexcept = default;
-
-      Task<void> get_return_object() noexcept;
-
-      void return_void() noexcept {}
-
-      void unhandled_exception() noexcept { exception = std::current_exception(); }
-
-      void result()
-      {
-        if (exception)
-          std::rethrow_exception(exception);
-      }
-    };
-
-    template<typename T>
-    class TaskPromise<T&> : public TaskPromiseBase
+    /// @brief Returns the result, or throws the unhandled exception
+    /// @return The returned object
+    T&& result() &&
     {
-      T* value = nullptr;
-      std::exception_ptr exception;
+      if (state == StorageState::EXCEPT)
+        std::rethrow_exception(get_exception());
+      return std::move(get_value());
+    }
+  };
 
-    public:
-      TaskPromise() noexcept = default;
+  template<typename PROMISE_BASE>
+  class generic_task_promise<void, PROMISE_BASE> : public PROMISE_BASE
+  {
+    std::exception_ptr exception;
 
-      Task<T&> get_return_object() noexcept;
+  public:
+    generic_task_promise() noexcept = default;
 
-      void unhandled_exception() noexcept { exception = std::current_exception(); }
-      void return_value(T& val) noexcept { value = &val; }
+    using return_type =
+        typename return_object_for_promise_base<PROMISE_BASE>::template type<void>;
+    return_type get_return_object() noexcept
+    {
+      using promise_t = generic_task_promise<void, PROMISE_BASE>;
+      auto h          = std::coroutine_handle<promise_t>::from_promise(*this);
+      return return_type{h};
+    }
 
-      T& result()
+    void return_void() noexcept {}
+
+    void unhandled_exception() noexcept { exception = std::current_exception(); }
+
+    void result()
+    {
+      if (exception)
+        std::rethrow_exception(exception);
+    }
+  };
+
+  template<typename T, typename PROMISE_BASE>
+  class generic_task_promise<T&, PROMISE_BASE> : public PROMISE_BASE
+  {
+    T* value = nullptr;
+    std::exception_ptr exception;
+
+  public:
+    generic_task_promise() noexcept = default;
+
+    using return_type =
+        typename return_object_for_promise_base<PROMISE_BASE>::template type<T&>;
+    return_type get_return_object() noexcept
+    {
+      using promise_t = generic_task_promise<T&, PROMISE_BASE>;
+      auto h          = std::coroutine_handle<promise_t>::from_promise(*this);
+      return return_type{h};
+    }
+
+    void unhandled_exception() noexcept { exception = std::current_exception(); }
+    void return_value(T& val) noexcept { value = &val; }
+
+    T& result()
+    {
+      if (exception)
+        std::rethrow_exception(exception);
+      return *value;
+    }
+  };
+
+  //--------------------------------------
+  // SINGLE OWNING, SYNCHRONOUS `Task<T>`
+  //--------------------------------------
+
+  /// @brief Promise base for `Task<T>`
+  struct TaskPromiseBase
+  {
+    /// @brief The coroutine to continue after the final suspension point
+    std::coroutine_handle<> continuation;
+
+    /// @brief Awaitable for `final_suspend`
+    struct final_awaitable
+    {
+      /// @brief Never ready
+      bool await_ready() const noexcept { return false; }
+
+      /// @brief Returns the continuation or `std::noop_coroutine` if there isn't any
+      /// @tparam PROMISE The promise type
+      /// @param handle The coroutine handle
+      /// @return The continuation
+      template<typename PROMISE>
+      std::coroutine_handle<> await_suspend(
+          std::coroutine_handle<PROMISE> handle) const noexcept
       {
-        if (exception)
-          std::rethrow_exception(exception);
-        return *value;
+        auto ret = handle.promise().continuation;
+        return ret ? ret : std::noop_coroutine();
       }
+
+      /// @brief Does nothing
+      void await_resume() const noexcept {}
     };
-  } // namespace detail
+
+    /// @brief Initially suspended
+    auto initial_suspend() const noexcept { return std::suspend_always{}; }
+    /// @brief Starts the continuation
+    auto final_suspend() const noexcept { return final_awaitable{}; }
+  };
 
   template<typename T = void>
   struct Task
   {
-    using promise_type = detail::TaskPromise<T>;
+    using promise_type = generic_task_promise<T, TaskPromiseBase>;
     using value_type   = T;
 
   private:
@@ -204,7 +255,7 @@ namespace stdcolt::coroutines
       std::coroutine_handle<> await_suspend(
           std::coroutine_handle<> coroutine) noexcept
       {
-        handle.promise().set_continuation(coroutine);
+        handle.promise().continuation = coroutine;
         return handle;
       }
     };
@@ -303,25 +354,13 @@ namespace stdcolt::coroutines
     }
   };
 
-  namespace detail
+  /// @brief For TaskPromiseBase, a Task<T> is returned
+  template<>
+  struct return_object_for_promise_base<TaskPromiseBase>
   {
     template<typename T>
-    Task<T> TaskPromise<T>::get_return_object() noexcept
-    {
-      return Task<T>{std::coroutine_handle<TaskPromise>::from_promise(*this)};
-    }
-
-    inline Task<void> TaskPromise<void>::get_return_object() noexcept
-    {
-      return Task<void>{std::coroutine_handle<TaskPromise>::from_promise(*this)};
-    }
-
-    template<typename T>
-    Task<T&> TaskPromise<T&>::get_return_object() noexcept
-    {
-      return Task<T&>{std::coroutine_handle<TaskPromise>::from_promise(*this)};
-    }
-  } // namespace detail
+    using type = Task<T>;
+  };
 } // namespace stdcolt::coroutines
 
 #endif // !__HG_STDCOLT_COROUTINES_TASK
