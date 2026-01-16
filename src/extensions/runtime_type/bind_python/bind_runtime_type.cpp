5b941af754ebcb995721e4460165a285144c9b3e
R533-Code <raphaeldibnehmepc@gmail.com>
2026-01-16 12:17:02 +0100

    Added bind_python directory

---
+++ src/extensions/runtime_type/bind_python/bind_runtime_type.cpp
@@ -0,0 +1,14 @@
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/make_iterator.h>
+#include <nanobind/eval.h>
+
+#include <stdcolt_runtime_type/cpp/runtime_type.h>
+
+void bind_python_bind_runtime_type(nanobind::module_& mod)
+{
+  namespace nb = nanobind;
+  using namespace stdcolt::ext::rt;
+
+  nb::class_<Value>(mod, "Value");
+}
